55c029de843ec1e3551becd229a599f3e6e10b4b
Michael Kennedy <mikeckennedy@gmail.com>
2017-02-02 10:58:29 -0800

    Demo code for accessing GitHub API.

--- services/README.md
+++ services/README.md
@@ -6,7 +6,7 @@ I'm just including the code so you have a local version.
 
 If you want to run the local version, it's a standard Pyramid app so run it as follows:
 
->> python3 -m venv ~/sevices_env
->> . ~/sevices_env/bin/activate
->> python consuming_services_apis/setup.py develop
->> serve consuming_services_apis/development.ini
+    >> python3 -m venv --copies ~/sevices_env
+    >> . ~/sevices_env/bin/activate
+    >> python consuming_services_apis/setup.py develop
+    >> serve consuming_services_apis/development.ini
---
+++ src/ch2_http_services/githib_api_from_python.py
@@ -0,0 +1,37 @@
+import requests
+
+
+def main():
+    # mikeckennedy
+    # consuming_services_python_demos
+    
+    user, repo = get_repo_info()
+
+    url = 'https://api.github.com/repos/{}/{}'.format(
+        user, repo
+    )
+
+    resp = requests.get(url)
+
+    if resp.status_code != 200:
+        print("Error accessing repo: {}".format(resp.status_code))
+        return
+
+    repo_data = resp.json()
+    clone = repo_data.get('clone_url', 'ERROR: NO DATA')
+
+    print("To clone {}'s repo named {}".format(user, repo))
+    print("The command is: ")
+    print()
+    print("git clone {}".format(clone))
+
+
+def get_repo_info():
+    user = input("What is the username? ")
+    repo = input("What is the repo name? ")
+
+    return user, repo
+
+
+if __name__ == '__main__':
+    main()
