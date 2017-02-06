e6c3f735906a0c6d78749e16a63164888691de21
Michael Kennedy <mikeckennedy@gmail.com>
2017-02-06 13:49:37 -0800

    A very simple, tool driven client (c# example)

---
+++ src/ch3_soap_services/csharp_client/tooled_blog_consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tooled_blog_consumer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new svc.Blog();
+			var posts = client.AllPosts();
+			Console.WriteLine("All blog posts:");
+			foreach (var post in posts)
+			{
+				Console.WriteLine("* {0} has {1:N0} of views.", post.Title, post.ViewCount);
+			}
+
+		}
+	}
+}
